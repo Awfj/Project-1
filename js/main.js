172b7c469ecc7173315e3ec5c4d607a08686df6b
Awfj <awfj837@gmail.com>
2017-06-27 21:49:50 +0300

    added plugins

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
 	$(document).click(function() {
 		$('#main_nav').removeClass('active_menu');
 	});
-	
 
 	$('nav').find('a').click(function(){
 	    var $href = $(this).attr('href');
@@ -19,6 +18,12 @@ $(document).ready(function() {
 	//banner - full height banner
 	var winHeight = $(window).height() - $('#banner').offset().top;
 	$('#banner').css('height', winHeight);
+
+	$(window).on('scroll', function(){
+		if ($(window).scrollTop() >= winHeight){
+			$('#banner').css('height', 'initial');
+		}
+	});
 
 	//banner - scroll down
 	$('.scroll_down').click(function() {
@@ -34,30 +39,64 @@ $(document).ready(function() {
 	$('.filter li[data-design]').click(function() {
 		var design = $(this).data().design;
 		if (design == 'all') {
-			$('img[data-design]').show();
+			$('.icon[data-design]').show();
 			$('li[data-design='+ design +']').addClass('active');
 			$('li[data-design!='+ design +']').removeClass('active');
 		} else {
-			$('img[data-design='+ design +']').show();
+			$('.icon[data-design='+ design +']').show();
 			$('li[data-design='+ design +']').addClass('active');
 
-			$('#icons img[data-design!='+ design +']').hide();
+			$('.icon[data-design!='+ design +']').hide();
 			$('li[data-design!='+ design +']').removeClass('active');
 
 		}
 	});
 
-	//contact - inner social
-	$('.img').mouseenter(function() {
-		$('.inner_social', this).addClass('fade').css('display', 'block');
+	//lightbox
+	$('.icon').mouseenter(function() {
+		$('.inner_all', this).fadeIn(400);
 	});
-	$('.img').mouseleave(function() {
-		$('.inner_social', this).fadeOut(300);
+	$('.icon').mouseleave(function() {
+		$('.inner_all', this).fadeOut(400);
 	});
 
-	//fotter - scroll up
+	//contact - inner social
+	$('.img').mouseenter(function() {
+		$('.inner_social', this).fadeIn(500);
+	});
+	$('.img').mouseleave(function() {
+		$('.inner_social', this).fadeOut(500);
+	});
+
+	//footer - scroll up
 	$('#toTop').click(function() {
 		$('body, html').animate({scrollTop: 0}, 800);
 		return false;
+	});
+
+	lightbox.option({
+		'positionFromTop': 200
+	});
+	$(".owl-carousel").owlCarousel({
+		responsiveClass:true,
+		mouseDrag: true,
+		responsive:{
+		        0:{
+		            items:1,
+		            nav:true,
+		            loop: true
+		        },
+		        350:{
+		            items:2,
+		            nav:true,
+		            loop: true
+		        },
+		        600:{
+		            items:3,
+		            nav:false,
+		            loop: false,
+		            mouseDrag: false
+		        }
+		    }
 	});
 });
